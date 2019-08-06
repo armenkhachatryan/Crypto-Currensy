d9e9eb249d7f462a32cdbf096464074735d2526d
Armen <menchkhachatryan@gmail.com>
2019-08-06 15:01:55 +0400

    Creating new React App

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Header from './components/common/Header';
+import List from './components/list/List';
+import NotFound from './components/notfound/NotFound';
+import Detail from './components/detail/Detail';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = () => {  
+  return (
+    <BrowserRouter>
+      <div>
+        <Header />
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+        <Switch>
+          <Route path="/" component={List} exact />
+          <Route path="/currency/:id" component={Detail} exact />
+          <Route component={NotFound} />
+        </Switch>  
+      </div>
+    </BrowserRouter>
+  );
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
